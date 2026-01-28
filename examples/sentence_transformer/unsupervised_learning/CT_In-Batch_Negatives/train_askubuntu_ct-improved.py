8e12043d2aec1b1f59fc77007d5a299843280c04
Omkar Kabde <omkarkabde@gmail.com>
2026-01-28 21:07:31 +0530

    migrate `train_askubuntu_ct.py` from v2 to v3 and clean comments

--- examples/sentence_transformer/unsupervised_learning/CT_In-Batch_Negatives/train_askubuntu_ct-improved.py
+++ examples/sentence_transformer/unsupervised_learning/CT_In-Batch_Negatives/train_askubuntu_ct-improved.py
@@ -3,31 +3,43 @@ import logging
 import os
 from datetime import datetime
 
-from torch.utils.data import DataLoader
+from datasets import Dataset
 
-from sentence_transformers import InputExample, LoggingHandler, SentenceTransformer, evaluation, losses, models, util
-
-#### Just some code to print debug information to stdout
-logging.basicConfig(
-    format="%(asctime)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S", level=logging.INFO, handlers=[LoggingHandler()]
+from sentence_transformers import (
+    LoggingHandler,
+    SentenceTransformer,
+    evaluation,
+    losses,
+    models,
+    util,
 )
-#### /print debug information to stdout
+from sentence_transformers.trainer import SentenceTransformerTrainer
+from sentence_transformers.training_args import SentenceTransformerTrainingArguments
+
+# Just some code to print debug information to stdout
+logging.basicConfig(
+    format="%(asctime)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.INFO,
+    handlers=[LoggingHandler()],
+)
+# /print debug information to stdout
 
 # Some training parameters. We use a batch size of 16, for every positive example we include 8-1=7 negative examples
 # Sentences are truncated to 75 word pieces
-## Training parameters
+# Training parameters
 model_name = "distilbert-base-uncased"
 batch_size = 128
 epochs = 1
 max_seq_length = 75
 
-################# Download AskUbuntu and extract training corpus  #################
+# Download AskUbuntu and extract training corpus
 askubuntu_folder = "askubuntu"
 output_path = "output/train_askubuntu_ct-improved-{}-{}-{}".format(
     model_name, batch_size, datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 )
 
-## Download the AskUbuntu dataset from https://github.com/taolei87/askubuntu
+# Download the AskUbuntu dataset from https://github.com/taolei87/askubuntu
 for filename in ["text_tokenized.txt.gz", "dev.txt", "test.txt", "train_random.txt"]:
     filepath = os.path.join(askubuntu_folder, filename)
     if not os.path.exists(filepath):
@@ -72,48 +84,60 @@ dev_dataset = read_eval_dataset(os.path.join(askubuntu_folder, "dev.txt"))
 test_dataset = read_eval_dataset(os.path.join(askubuntu_folder, "test.txt"))
 
 
-## Now we need a list of train sentences.
-## In this example we simply use all sentences that don't appear in the train/dev set
+# Now we need a list of train sentences.
+# In this example we simply use all sentences that don't appear in the train/dev set
 train_sentences = []
 for id, sentence in corpus.items():
     if id not in dev_test_ids:
-        train_sentences.append(InputExample(texts=[sentence, sentence]))
+        train_sentences.append({"sentence1": sentence, "sentence2": sentence})
 
 logging.info(f"{len(train_sentences)} train sentences")
+train_dataset = Dataset.from_list(train_sentences)
 
-################# Initialize an SBERT model #################
-
+# Initialize an SBERT model
 word_embedding_model = models.Transformer(model_name, max_seq_length=max_seq_length)
 pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension())
 model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
 
-################# Train the model #################
-
-# For ContrastiveTension we need a special data loader to construct batches with the desired properties
-train_dataloader = DataLoader(train_sentences, batch_size=batch_size, shuffle=True, drop_last=True)
-
-# As loss, we losses.ContrastiveTensionLoss
+# Loss
 train_loss = losses.ContrastiveTensionLossInBatchNegatives(model)
 
-# Create a dev evaluator
+# Evaluators
 dev_evaluator = evaluation.RerankingEvaluator(dev_dataset, name="AskUbuntu dev")
 test_evaluator = evaluation.RerankingEvaluator(test_dataset, name="AskUbuntu test")
 
 
 logging.info("Start training")
 
-model.fit(
-    train_objectives=[(train_dataloader, train_loss)],
-    evaluator=dev_evaluator,
-    evaluation_steps=100,
-    epochs=1,
+# Prepare the training arguments
+args = SentenceTransformerTrainingArguments(
+    output_dir=output_path,
+    num_train_epochs=epochs,
+    per_device_train_batch_size=batch_size,
     warmup_steps=100,
-    use_amp=True,  # Set to True, if your GPU has optimized FP16 cores
+    learning_rate=5e-5,
+    eval_steps=100,
+    save_strategy="no",
+    fp16=True,
+    logging_steps=50,
 )
+
+
+# Train the model
+trainer = SentenceTransformerTrainer(
+    model=model,
+    args=args,
+    train_dataset=train_dataset,
+    evaluator=dev_evaluator,
+    loss=train_loss,
+)
+
+logging.info("Start training")
+trainer.train()
 
 latest_output_path = output_path + "-latest"
 model.save(latest_output_path)
 
-### Run test evaluation on the latest model. This is equivalent to not having a dev dataset
+# Run test evaluation on the latest model. This is equivalent to not having a dev dataset
 model = SentenceTransformer(latest_output_path)
 test_evaluator(model)
