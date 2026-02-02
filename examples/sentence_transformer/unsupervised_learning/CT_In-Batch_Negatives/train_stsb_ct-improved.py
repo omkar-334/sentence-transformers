2bcb7db068fa0e64f4555b65a299a3a35080c0ee
Tom Aarsen <Cubiegamedev@gmail.com>
2026-02-02 14:23:09 +0100

    Final nits, looks good now

--- examples/sentence_transformer/unsupervised_learning/CT_In-Batch_Negatives/train_stsb_ct-improved.py
+++ examples/sentence_transformer/unsupervised_learning/CT_In-Batch_Negatives/train_stsb_ct-improved.py
@@ -30,28 +30,25 @@ model_save_path = "output/training_stsb_ct-improved-{}-{}".format(
 
 # Train sentences
 # We use 1 Million sentences from Wikipedia to train our model
-wikipedia_dataset = load_dataset("sentence-transformers/wiki1m-for-simcse", split="train")
-
-train_dataset = wikipedia_dataset.map(lambda x: {"text": x["text"].strip()}, remove_columns=["text"])
-
+train_dataset = load_dataset("sentence-transformers/wiki1m-for-simcse", split="train[:1000]")
+train_dataset = train_dataset.map(lambda x: {"text": x["text"].strip()})
+print(train_dataset)
 
 # Download and load STSb
-sts_dataset = load_dataset("sentence-transformers/stsb")
-dev = sts_dataset["validation"]
-test = sts_dataset["test"]
-
+dev_sts_dataset = load_dataset("sentence-transformers/stsb", split="validation")
+test_sts_dataset = load_dataset("sentence-transformers/stsb", split="test")
 
 dev_evaluator = EmbeddingSimilarityEvaluator(
-    dev["sentence1"],
-    dev["sentence2"],
-    dev["score"],
+    dev_sts_dataset["sentence1"],
+    dev_sts_dataset["sentence2"],
+    dev_sts_dataset["score"],
     name="sts-dev",
 )
 
 test_evaluator = EmbeddingSimilarityEvaluator(
-    test["sentence1"],
-    test["sentence2"],
-    test["score"],
+    test_sts_dataset["sentence1"],
+    test_sts_dataset["sentence2"],
+    test_sts_dataset["score"],
     name="sts-test",
 )
 
@@ -68,7 +65,8 @@ args = SentenceTransformerTrainingArguments(
     output_dir=model_save_path,
     num_train_epochs=epochs,
     per_device_train_batch_size=batch_size,
-    warmup_steps=0.1,
+    warmup_ratio=0.1,
+    eval_strategy="steps",
     eval_steps=0.1,
     logging_steps=0.01,
     learning_rate=5e-5,
@@ -87,18 +85,21 @@ trainer = SentenceTransformerTrainer(
 
 # Train the model
 trainer.train()
+
+dev_evaluator(model)
 test_evaluator(model)
 
-model.save_pretrained(model_save_path)
+final_output_path = model_save_path + "-final"
+model.save_pretrained(final_output_path)
 
 # (Optional) save the model to the Hugging Face Hub!
 # It is recommended to run `huggingface-cli login` to log into your Hugging Face account first
 model_name = model_name if "/" not in model_name else model_name.split("/")[-1]
 try:
-    model.push_to_hub(f"{model_name}-simcse")
+    model.push_to_hub(f"{model_name}-stsb-ct")
 except Exception:
     logging.error(
         f"Error uploading model to the Hugging Face Hub:\nTo upload it manually, you can run "
-        f"`huggingface-cli login`, followed by loading the model using `model = SentenceTransformer({model_save_path!r})` "
+        f"`huggingface-cli login`, followed by loading the model using `model = SentenceTransformer({final_output_path!r})` "
         f"and saving it using `model.push_to_hub('{model_name}-stsb-ct')`."
     )
